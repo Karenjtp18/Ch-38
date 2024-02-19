2985e87f15e5c8d4b973e52edcfc70d7db041924
Karenjtp18 <karenjtp18@gmail.com>
2024-02-19 12:29:38 -0700

    funcion dime tu nombre

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hola Ch38");
+
+function saludo() {
+  alert("oli");
+}
+
+/**
+ * Pedir el nombre de la persona usando un Promt()
+ * Mostrar un saludo y nombre de la persona con alert
+ */
+function saludarPersona() {
+  let nombre = prompt("Dime tu nombre");
+  alert(`Hola ${nombre}`);
+}
